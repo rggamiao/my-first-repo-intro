9f824d4708c90121f95296986031afbcad3b21d3
rggamiao <158120158+rggamiao@users.noreply.github.com>
2024-01-29 20:51:04 -0800

    Update index.js

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".image1, .menu-section, #favorites, .locationhours").hide();
+    
+    $(".image1").delay(300).fadeIn(5000)
+      ;
+      
+      $(".menu-section").delay(300).fadeIn(5000);
+    
+    $("#favorites").delay(300).fadeIn(5000);
+   
+    $(".locationhours").delay(300).fadeIn(5000)
+  });
